aae9d550698767ee409e09957d191fcdf1ac6dba
Filip Milanko <filip.milanko.developer@gmail.com>
2022-02-13 12:55:12 +0100

    Fixed release build issues
    Fixed lint issue.
    Fixed serialization issue of GetUsersAverageCaloriesDTO object.

--- calorie-guide-android-client/calorieguide-api/src/main/java/com/example/api/model/responses/GetUsersAverageCaloriesDTO.kt
+++ calorie-guide-android-client/calorieguide-api/src/main/java/com/example/api/model/responses/GetUsersAverageCaloriesDTO.kt
@@ -1,5 +1,7 @@
 package com.example.api.model.responses
 
+import androidx.annotation.Keep
 import com.example.api.model.UserAverageCaloriesDTO
 
+@Keep
 data class GetUsersAverageCaloriesDTO(val items: List<UserAverageCaloriesDTO>?)
